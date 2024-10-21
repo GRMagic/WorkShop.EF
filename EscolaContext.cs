c312630a04857c5a954d7a1054efba5f7eba276a
Gustavo Rossi Müller <gustavo.rm90@gmail.com>
2024-10-21 10:01:56 -0300

    Add project files.

---
+++ EscolaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkShop.EF.Models;
+
+namespace WorkShop.EF
+{
+    public class EscolaContext : DbContext
+    {
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle(@"User Id=workshop_ef_gustavo;Password=teste123;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=172.16.25.135)(Port=1521))(CONNECT_DATA=(SERVICE_NAME=dev19c31.supero.com.br)))");
+        }
+
+        public DbSet<Estudante> Estudantes { get; set; }
+
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkShop.EF.Models
+{
+    public class Estudante
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+}
