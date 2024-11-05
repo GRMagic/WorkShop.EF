0d5808778cece3ea0fdeffc4839fcad30bd9c1a8
Gustavo Rossi Müller <gustavo.rm90@gmail.com>
2024-11-05 15:00:12 -0300

    Imprimindo os logs do EFCore no console

--- EscolaContext.cs
+++ EscolaContext.cs
@@ -5,6 +5,10 @@ public class EscolaContext : DbContext
     override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseOracle(@"User Id=workshop_ef_gustavo;Password=teste123;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=172.16.25.135)(Port=1521))(CONNECT_DATA=(SERVICE_NAME=dev19c31.supero.com.br)))");
+        
+        optionsBuilder.EnableDetailedErrors();
+        optionsBuilder.EnableSensitiveDataLogging();
+        optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
     }
 
     public DbSet<Estudante> Estudantes { get; set; }
