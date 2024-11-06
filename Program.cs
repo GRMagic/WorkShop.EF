dffc8c6b0350df9fc34957af6c2d06d644df3461
Gustavo Rossi Müller <gustavo.rm90@gmail.com>
2024-11-06 09:01:22 -0300

    Uso de Include, OrderBy e ThenBy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using var context = new EscolaContext();
+﻿using WorkShop.EF.Models;
+
+using var context = new EscolaContext();
 
 context.Database.EnsureDeleted();
 
@@ -6,42 +8,43 @@ context.Database.EnsureDeleted();
 
 context.Database.Migrate();
 
-// Criação de um novo estudante
-var novoEstudante = new Estudante()
 {
-    Nome = "Fulano",
-    Sobrenome = "De Tal",
-};
+    // Criação de um novo estudante
+    var novoEstudante = new Estudante()
+    {
+        Nome = "Fulano",
+        Sobrenome = "De Tal",
+    };
 
-context.Estudantes.Add(novoEstudante);
+    context.Estudantes.Add(novoEstudante);
 
-context.SaveChanges();
+    context.SaveChanges();
 
-// Listagem de estudantes
+    // Listagem de estudantes
 
-var estudantes = context.Estudantes.ToList();
+    var estudantes = context.Estudantes.ToList();
 
-foreach (var estudante in estudantes)
-    Console.WriteLine($"{estudante.Id:00}: {estudante.Nome} {estudante.Sobrenome}");
+    foreach (var estudante in estudantes)
+        Console.WriteLine($"{estudante.Id:00}: {estudante.Nome} {estudante.Sobrenome}");
 
-// Atualização de um estudante
+    // Atualização de um estudante
 
-estudantes[0].Nome = "Sicrano";
+    estudantes[0].Nome = "Sicrano";
 
-context.SaveChanges();
+    context.SaveChanges();
 
-// Busca de um estudante
+    // Busca de um estudante
 
-var sicrano = context.Estudantes.First(e => e.Nome == "Sicrano");
+    var sicrano = context.Estudantes.First(e => e.Nome == "Sicrano");
 
-Console.WriteLine($"Id: {sicrano.Id}, Nome: {sicrano.Nome} {sicrano.Sobrenome}");
+    Console.WriteLine($"Id: {sicrano.Id}, Nome: {sicrano.Nome} {sicrano.Sobrenome}");
 
-// Remoção de um estudante
+    // Remoção de um estudante
 
-context.Estudantes.Remove(sicrano);
+    context.Estudantes.Remove(sicrano);
 
-context.SaveChanges();
-
+    context.SaveChanges();
+}
 
 // Inclusão de um estudante duplicado
 //try
@@ -66,28 +69,44 @@ context.SaveChanges();
 //}
 
 // Exemplo inclusão de várias entidades usando um comando só
-
-var curso = new Curso()
 {
-    Nome = "Workshop EF Core"
-};
+    var curso = new Curso()
+    {
+        Nome = "Workshop EF Core"
+    };
 
-var renato = new Estudante()
+    var renato = new Estudante()
+    {
+        Nome = "Renato",
+        Sobrenome = "Russo"
+    };
+
+    var matricula = new Matricula()
+    {
+        Estudante = renato,
+        Curso = curso,
+        Data = DateTime.Now
+    };
+
+    // Adiciona o curso, o estudante e a matrícula em um comando só
+
+    context.Matriculas.Add(matricula);
+
+    context.SaveChanges();
+}
+
+// Exemplo de consulta com join
 {
-    Nome = "Renato",
-    Sobrenome = "Russo"
-};
+    var matriculas = context.Matriculas
+        .Include(m => m.Estudante)
+        .Include(m => m.Curso)
+        .OrderBy(m => m.Curso.Nome)
+        .ThenBy(m => m.Estudante.Nome)
+        .ToList();
 
-var matricula = new Matricula()
-{
-    Estudante = renato,
-    Curso = curso,
-    Data = DateTime.Now
-};
-
-// Adiciona o curso, o estudante e a matrícula em um comando só
-
-context.Matriculas.Add(matricula);
-
-context.SaveChanges();
-
+    const int largura = -40;
+    Console.WriteLine($"\n{"Curso",largura} : Aluno");
+    foreach (var matricula in matriculas)
+        Console.WriteLine($"{matricula.Curso.Nome,largura} : {matricula.Estudante.Nome}");
+    
+}
